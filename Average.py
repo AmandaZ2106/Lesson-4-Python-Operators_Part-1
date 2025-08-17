90da652da4351d6b6faf73870332444a582f18ee
AmandaZ2106 <romeogcruz@outlook.com>
2025-08-17 17:21:28 +0100

    Average Age of Family

---
+++ Average.py
@@ -0,0 +1,7 @@
+Dad=43
+Mom=35
+Me=12
+Ayden=5
+print("The sum of the ages of everyone in my family=",(Dad+Mom+Me+Ayden))
+Sum=Dad+Mom+Ayden+Me
+print("The average age of everyone in my family=",Sum//4)
