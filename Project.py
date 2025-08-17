ffe0b52baa92724fae1ca9cb12c7d327f701c564
AmandaZ2106 <romeogcruz@outlook.com>
2025-08-17 18:19:51 +0100

    Project

---
+++ Project.py
@@ -0,0 +1,8 @@
+from math import sqrt
+
+
+name=input("What is your name?")
+print("Hello",name,)
+no=int(input("What number do you require me to find the square root of?"))
+result = sqrt(no)
+print(f"The square root of {no} is: {result}")
